7761fe52539c54edb64965c4ff9a79040f30684e
guqing <1484563614@qq.com>
2020-05-08 15:18:25 +0800

    feat: 新增测试微服务模块

---
+++ violet-microservice/microservice-test/src/main/java/xyz/guqing/micro/test/MicroTestApplication.java
@@ -0,0 +1,15 @@
+package xyz.guqing.micro.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author guqing
+ * @date 2020-05-08
+ */
+@SpringBootApplication
+public class MicroTestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MicroTestApplication.class, args);
+    }
+}

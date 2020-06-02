b9970727aa8d0db65598a1fd0e3891a1cb4cb2f1
guqing <38999863+guqing@users.noreply.github.com>
2020-06-02 23:21:47 +0800

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,14 +3,14 @@
 serverIp=$1
 pwd=$2
 # 上传jar
-sshpass -p $pwd scp -P 22 -o StrictHostKeyChecking=no violet-gateway/target/violet-gateway-0.0.1-SNAPSHOT.jar root@$serverIp:/root/violet/gateway
-sshpass -p $pwd scp -P 22 -o StrictHostKeyChecking=no violet-auth/target/violet-auth-0.0.1-SNAPSHOT.jar root@$serverIp:/root/violet/auth
-sshpass -p $pwd scp -P 22 -o StrictHostKeyChecking=no violet-app/violet-app-admin/target/violet-app-admin-0.0.1-SNAPSHOT.jar root@$serverIp:/root/violet/app/admin
+sshpass -p $pwd scp violet-gateway/target/violet-gateway-0.0.1-SNAPSHOT.jar root@$serverIp:/root/violet/gateway
+sshpass -p $pwd scp violet-auth/target/violet-auth-0.0.1-SNAPSHOT.jar root@$serverIp:/root/violet/auth
+sshpass -p $pwd scp violet-app/violet-app-admin/target/violet-app-admin-0.0.1-SNAPSHOT.jar root@$serverIp:/root/violet/app/admin
 # 执行脚本
 sshpass -p $pwd ssh -tt -p 22 -o StrictHostKeyChecking=no root@$serverIp 2>&1 << eeooff
-cd /root/violet/gateway
-echo '开始执行服务器命令'
+echo '开始在目标服务器执行脚本'
 source /etc/profile ~/.profile ~/.bash_profile
+cd /root/violet/gateway 
 ./stop.sh
 ./run.sh
 cd /root/violet/auth

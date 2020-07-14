812ec165da374526fb99268cda29e4b0a38b72fe
guqing <1484563614@qq.com>
2020-07-14 15:07:22 +0800

    feat: 新增MailService

---
+++ violet-common/violet-common-core/src/main/java/xyz/guqing/violet/common/core/notify/service/MailService.java
@@ -0,0 +1,125 @@
+package xyz.guqing.violet.common.core.notify.service;
+
+import freemarker.template.Template;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import javax.mail.internet.MimeMessage;
+
+/**
+ * @author guqing
+ * @date 2020-07-14
+ */
+@Service
+public class MailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
+    //发送邮件的模板引擎
+    @Autowired
+    private FreeMarkerConfigurer freeMarkerConfigurer;
+
+    public String sendSimpleMail() {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(from);
+            message.setTo("888888@qq.com");
+            message.setSubject("一封简单的邮件");
+            message.setText("使用Spring Boot发送简单邮件。");
+            javaMailSender.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    public String sendMailWithHtml() {
+        MimeMessage message = null;
+        try {
+            message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo("888888@qq.com"); // 接收地址
+            helper.setSubject("一封HTML格式的邮件"); // 标题
+            // 带HTML格式的内容
+            StringBuffer sb = new StringBuffer("<p style='color:#42b983'>使用Spring Boot发送HTML格式邮件。</p>");
+            helper.setText(sb.toString(), true);
+            javaMailSender.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    public String sendMailWithAttachments() {
+        MimeMessage message = null;
+        try {
+            message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo("888888@qq.com"); // 接收地址
+            helper.setSubject("一封带附件的邮件"); // 标题
+            helper.setText("详情参见附件内容！"); // 内容
+            // 传入附件
+            FileSystemResource file = new FileSystemResource(new File("src/main/resources/static/file/项目文档.docx"));
+            helper.addAttachment("项目文档.docx", file);
+            javaMailSender.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    /**
+     * 发送带静态资源的邮件其实就是在发送HTML邮件的基础上嵌入静态资源（比如图片
+     * 嵌入静态资源的过程和传入附件类似，唯一的区别在于需要标识资源的cid：
+     */
+    public String sendInlineMail() {
+        MimeMessage message = null;
+        try {
+            message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo("888888@qq.com"); // 接收地址
+            helper.setSubject("一封带静态资源的邮件"); // 标题
+            helper.setText("<html><body>博客图：<img src='cid:img'/></body></html>", true); // 内容
+            // 传入附件
+            FileSystemResource file = new FileSystemResource(new File("src/main/resources/static/img/sunshine.png"));
+            helper.addInline("img", file);
+            jms.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+
+    public String sendMailWithTemplate(String code) {
+        MimeMessage message = null;
+        try {
+            message = javaMailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo("888888@qq.com"); // 接收地址
+            helper.setSubject("邮件摸板测试"); // 标题
+            // 处理邮件模板
+            Template template = freeMarkerConfigurer.getConfiguration().getTemplate("templateName");
+            // 填充参数
+            String text = FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
+            helper.setText(text, true);
+            javaMailSender.send(message);
+            return "发送成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+    }
+}

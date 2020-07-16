4d03c776be5ee77eb23caac3345fb9891253538f
guqing <1484563614@qq.com>
2020-07-16 15:36:40 +0800

    refactor: 移除local date convert并修改日期格式

--- violet-app/violet-app-admin/src/main/java/xyz/guqing/violet/app/admin/config/MyWebMvcConfigure.java
+++ violet-app/violet-app-admin/src/main/java/xyz/guqing/violet/app/admin/config/MyWebMvcConfigure.java
@@ -3,7 +3,6 @@ package xyz.guqing.violet.app.admin.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import xyz.guqing.violet.app.admin.converter.StringToLocalDateConverter;
 import xyz.guqing.violet.app.admin.converter.StringToLocalDateTimeConverter;
 
 /**
@@ -14,7 +13,6 @@ import xyz.guqing.violet.app.admin.converter.StringToLocalDateTimeConverter;
 public class MyWebMvcConfigure implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new StringToLocalDateConverter());
         registry.addConverter(new StringToLocalDateTimeConverter());
     }
 }

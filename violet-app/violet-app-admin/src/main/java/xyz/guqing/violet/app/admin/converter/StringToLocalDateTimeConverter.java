873942d0145404252e70f7ed51bb2fcff7137eed
guqing <1484563614@qq.com>
2020-07-16 17:52:40 +0800

    fix: 修复字符串转LocalDateTime出错问题

--- violet-app/violet-app-admin/src/main/java/xyz/guqing/violet/app/admin/converter/StringToLocalDateTimeConverter.java
+++ violet-app/violet-app-admin/src/main/java/xyz/guqing/violet/app/admin/converter/StringToLocalDateTimeConverter.java
@@ -5,7 +5,8 @@ import org.springframework.lang.NonNull;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.temporal.ChronoField;
 
 /**
  * @author guqing
@@ -13,11 +14,17 @@ import java.util.Locale;
  */
 public class StringToLocalDateTimeConverter implements Converter<String, LocalDateTime> {
     /**
-     * 因为 Spring 默认不支持将 String 类型的请求参数转换为 LocalDateTime 类型，所以我们需要自定义 converter 「转换器」完整整个转换过程
+     * 构建格式化器，并赋予可选参数默认值，否则会出错
      */
+    private static final DateTimeFormatter DATE_FORMAT = new DateTimeFormatterBuilder()
+            .appendPattern("yyyy-MM-dd[ [HH][:mm][:ss][.SSS]]")
+            .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+            .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
+            .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
+            .toFormatter();
+
     @Override
     public LocalDateTime convert(@NonNull String s) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd", Locale.CHINESE);
-        return LocalDateTime.parse(s, formatter);
+        return LocalDateTime.parse(s, DATE_FORMAT);
     }
 }

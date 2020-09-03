a9172c3d9b5e9af706311fbbf608d1037a1af9e5
guqing <1484563614@qq.com>
2020-09-03 15:26:37 +0800

    fix: 修复缺失passwordEncoder bean问题

--- violet-app/violet-app-admin/src/main/java/xyz/guqing/violet/app/admin/config/MyWebMvcConfigure.java
+++ violet-app/violet-app-admin/src/main/java/xyz/guqing/violet/app/admin/config/MyWebMvcConfigure.java
@@ -1,7 +1,10 @@
 package xyz.guqing.violet.app.admin.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import xyz.guqing.common.support.converter.StringToLocalDateTimeConverter;
 
@@ -14,5 +17,10 @@ public class MyWebMvcConfigure implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToLocalDateTimeConverter());
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }

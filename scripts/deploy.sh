fa13c75a392b93501c5264155b2ea4f8fcb02dd2
guqing <1484563614@qq.com>
2020-10-17 17:23:08 +0800

    fix: 修复部署脚本错误文件名导致寻找不到文件

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,9 @@ pwd=$2
 # 上传jar
 #sshpass -p $pwd scp violet-gateway/target/violet-gateway-0.0.1-SNAPSHOT.jar root@$serverIp:/root/violet/gateway
 ls violet-app/violet-app-admin/target/
-sshpass -p $pwd scp -P 22 -o StrictHostKeyChecking=no violet-app/violet-app-admin/target/violet-app-admin-0.0.1-SNAPSHOT.jar root@$serverIp:/root/violet/app/admin
-sshpass -p $pwd scp -P 22 -o StrictHostKeyChecking=no violet-auth/target/violet-auth-0.0.1-SNAPSHOT.jar root@$serverIp:/root/violet/auth
+sshpass -p $pwd scp -P 22 -o StrictHostKeyChecking=no violet-app/violet-app-admin/target/violet-app-admin.jar root@$serverIp:/root/violet/app/admin
+sshpass -p $pwd scp -P 22 -o StrictHostKeyChecking=no violet-auth/target/violet-auth.jar root@$serverIp:/root/violet/auth
+#sshpass -p $pwd scp -P 22 -o StrictHostKeyChecking=no violet-gateway/target/violet-gateway.jar root@$serverIp:/root/violet/gateway
 
 # 执行脚本
 sshpass -p $pwd ssh -tt -p 22 -o StrictHostKeyChecking=no root@$serverIp 2>&1 << eeooff

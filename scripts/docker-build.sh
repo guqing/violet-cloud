02dfe9f588d6b03b7148e8980be45db1ee68e5be
guqing <1484563614@qq.com>
2021-01-26 23:36:48 +0800

    fix: 修复docker build

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-image_version=${VERSION:latest}
+DOCKERHUB_USERNAME=$1
+DOCKERHUB_TOKEN=$2
+image_version=${3:=latest}
 docker_username="guching"
-#echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
+
+echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 # build violet-gateway
-docker build -f violet-gateway/Dockerfile  -t $docker_username/violet-gateway:"$image_version" ./violet-gateway
+docker build -f ../violet-gateway/Dockerfile  -t $docker_username/violet-gateway:"$image_version" ../violet-gateway
 
 # build violet-auth
-docker build -f violet-auth/Dockerfile  -t $docker_username/violet-auth:"$image_version" ./violet-auth
+docker build -f ../violet-auth/Dockerfile  -t $docker_username/violet-auth:"$image_version" ../violet-auth
 
 #build violet-app-admin
-violet_app_admin="violet-app/violet-app-admin"
+violet_app_admin="../violet-app/violet-app-admin"
 docker build -f $violet_app_admin/Dockerfile -t $docker_username/violet-app-admin:"$image_version" $violet_app_admin
 
+docker push $docker_username/violet-gateway:"$image_version"
+docker push $docker_username/violet-auth:"$image_version"
+docker push $docker_username/violet-app-admin:"$image_version"

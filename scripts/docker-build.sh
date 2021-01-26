2dfce1144c836af32fa45eebfc1859f10431251f
guqing <38999863+guqing@users.noreply.github.com>
2021-01-26 22:36:53 +0800

    Update docker-build.sh

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 image_version=${VERSION:latest}
 docker_username="guching"
+echo "--->$docker_username---->$image_version--->$DOCKERHUB_USERNAME"
 #echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 # build violet-gateway
 docker build -f violet-gateway/Dockerfile  -t $docker_username/violet-gateway:"$image_version" ./violet-gateway

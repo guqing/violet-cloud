911fe3280bce1d9ceb067f21ea65204c7e6c37ab
guqing <1484563614@qq.com>
2021-01-26 22:22:34 +0800

    fix: 修复docker build script

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 image_version=${VERSION:latest}
+docker_username="guching"
 #echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 # build violet-gateway
-docker build -f violet-gateway/Dockerfile  -t "$DOCKERHUB_USERNAME"/violet-gateway:"$image_version" ./violet-gateway
+docker build -f violet-gateway/Dockerfile  -t "$docker_username"/violet-gateway:"$image_version" ./violet-gateway
 
 # build violet-auth
-docker build -f violet-auth/Dockerfile  -t "$DOCKERHUB_USERNAME"/violet-auth:"$image_version" ./violet-auth
+docker build -f violet-auth/Dockerfile  -t "$docker_username"/violet-auth:"$image_version" ./violet-auth
 
 #build violet-app-admin
 violet_app_admin="violet-app/violet-app-admin"
-docker build -f $violet_app_admin/Dockerfile -t "$DOCKERHUB_USERNAME"/violet-app-admin:"$image_version" $violet_app_admin
+docker build -f $violet_app_admin/Dockerfile -t "$docker_username"/violet-app-admin:"$image_version" $violet_app_admin
 
